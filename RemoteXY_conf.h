c87da6c91a0109f4b1ea413aeb50f486212c7ed2
Volodymyr Tyshchenko <volodymyr@tyshchenko.de>
2025-12-02 10:46:19 +0100

    Update interface for RemoteXY

--- RemoteXY_conf.h
+++ RemoteXY_conf.h
@@ -18,11 +18,12 @@
 
 // RemoteXY GUI configuration  
 #pragma pack(push, 1)  
-uint8_t RemoteXY_CONF[] =   // 67 bytes
-  { 255,4,0,1,0,60,0,19,0,0,0,69,83,80,95,70,66,48,51,95,
-  49,52,45,67,0,26,1,106,200,1,1,4,0,5,2,8,60,60,32,16,
-  26,31,3,3,156,97,36,131,62,26,70,74,25,26,26,144,26,37,0,10,
-  73,88,24,24,0,4,26 };
+uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] =   // 92 bytes V19 
+  { 255,4,0,1,0,85,0,19,0,0,0,69,83,80,95,70,66,48,51,95,
+  49,52,45,67,0,26,1,200,84,1,1,4,0,5,13,20,60,60,32,16,
+  26,31,3,147,1,45,82,2,62,26,70,36,3,14,14,144,26,37,0,10,
+  97,4,20,20,48,4,26,24,83,101,110,115,111,114,115,32,79,78,0,31,
+  83,111,110,100,111,114,115,32,79,70,70,0 };
   
 // this structure defines all the variables and events of your control interface 
 struct {
@@ -30,7 +31,7 @@ struct {
     // input variables
   int8_t linearSpeed; // from -100 to 100
   int8_t angularSpeed; // from -100 to 100
-  uint8_t position; // from 0 to 2
+  uint8_t position; // from 0 to 1
   uint8_t sensorsON; // =1 if state is ON, else =0, from 0 to 1
 
     // output variables
